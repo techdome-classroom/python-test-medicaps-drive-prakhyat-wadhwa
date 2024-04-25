71703eaaf3a552496f1118445b47f7cc0826f347
prakhyat-wadhwa <168067540+prakhyat-wadhwa@users.noreply.github.com>
2024-04-25 14:01:40 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+         dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         result = 0
         prev = 0
         for char in s:
