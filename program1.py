62de5609f256f777fa4d84d0e28fd02c0eac5e8a
prakhyat-wadhwa <168067540+prakhyat-wadhwa@users.noreply.github.com>
2024-04-25 13:57:58 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        prev = 0
+        for char in s:
+            num = dict[char]
+            result += num
+            if num > prev:
+                result -= 2 * prev
+            prev = num
+        return result
     
 
 
